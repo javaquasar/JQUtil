2e2d36e437c0b99b9327a25ace9c6c612ec9d813
Java Quasar <java.quasar@gmail.com>
2017-08-09 13:15:53 +0300

    io

--- src/main/java/com/javaquasar/util/io/FileManager.java
+++ src/main/java/com/javaquasar/util/io/FileManager.java
@@ -4,10 +4,13 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -81,4 +84,36 @@ public class FileManager {
         }
         return prop;
     }
+    
+    
+    private static void copyFileUsingFileStreams(File source, File dest) throws IOException {
+        try (InputStream input = new FileInputStream(source); 
+                OutputStream output = new FileOutputStream(dest) ) {
+            byte[] buf = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = input.read(buf)) > 0) {
+                output.write(buf, 0, bytesRead);
+            }
+        }
+    }
+    
+    private static void copyFileUsingFileChannels(File source, File dest) throws IOException {
+        try (FileChannel inputChannel = new FileInputStream(source).getChannel();
+                FileChannel outputChannel = new FileOutputStream(dest).getChannel()) {
+            outputChannel.transferFrom(inputChannel, 0, inputChannel.size());
+        }
+    }
+    
+    private static void copyFileUsingJava7Files(File source, File dest) throws IOException {
+        Files.copy(source.toPath(), dest.toPath());
+    }
+    
+    public static void copyFile(File source, File dest) throws IOException {
+        copyFileUsingJava7Files(source, dest);
+    }
+    
+    public static void copyFile(String source, String dest) throws IOException {
+        copyFileUsingJava7Files(new File(source), new File(dest));
+    }   
+
 }
